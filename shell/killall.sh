0599b3b082d84741e1a8f6a7359b157d629b2ce1
liteng <liteng@elastos.org>
2019-04-22 16:40:37 +0800

    update killall.sh to kill all process

--- shell/killall.sh
+++ shell/killall.sh
@@ -1,3 +1,38 @@
 #!/bin/bash 
 
-killall ela java did arbiter
+#killall ela java did arbiter
+
+for i in `seq 50`
+do
+	ID=`ps -ef | grep "ela$i" | grep -v "grep" | awk '{print $2}'`
+	for id in $ID
+	do
+		kill -9 $id
+		echo "killed $id"
+	done
+done
+
+for i in `seq 10`
+do
+	ID=`ps -ef | grep "arbiter$i" | grep -v "grep" | awk '{print $2}'`
+	for id in $ID
+	do
+		kill -9 $id
+		echo "killed $id"
+	done
+done
+
+
+for i in `seq 10`
+do
+	ID=`ps -ef | grep "did$i" | grep -v "grep" | awk '{print $2}'`
+	for id in $ID
+	do
+		kill -9 $id
+		echo "killed $id"
+	done
+done
+
+killall java
+
+echo "kill all processes!"
