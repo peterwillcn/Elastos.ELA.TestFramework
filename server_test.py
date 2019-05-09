595aaef494add9567a250c9e25244595d5851b44
liteng <liteng@elastos.org>
2019-05-09 21:26:06 +0800

    update content in server_test.py

--- server_test.py
+++ server_test.py
@@ -11,23 +11,25 @@ from src.tools.log import Logger
 
 if __name__ == '__main__':
 
-    case1 = "dpos_normal_test"
-    case2 = "rotation_onebyone_test"
-    case3 = "rotation_whole_test"
-    case4 = "inactive_single_test"
-    case5 = "majority_inactive_1_turn"
-    case6 = "majority_inactive_2_turns"
-    case7 = "majority_inactive_crc_degradation"
-    case8 = "insufficient_producer_degradation_pre_connect"
-    case9 = "insufficient_producer_degradation_first_inactive"
-    case10 = "insufficient_producer_degradation_cancel_no_stop"
-    case11 = "insufficient_producer_degradation_cancel_and_stop"
-    case12 = "inactive_crc_test"
-    case13 = "cross_normal_test"
+    test_cases = dict()
+    test_cases["case1"] = "dpos_normal_test"
+    test_cases["case2"] = "rotation_onebyone_test"
+    test_cases["case3"] = "rotation_whole_test"
+    test_cases["case4"] = "inactive_single_test"
+    test_cases["case5"] = "majority_inactive_1_turn"
+    test_cases["case6"] = "majority_inactive_2_turns"
+    test_cases["case7"] = "majority_inactive_crc_degradation"
+    test_cases["case8"] = "insufficient_producer_degradation_pre_connect"
+    test_cases["case9"] = "insufficient_producer_degradation_first_inactive"
+    test_cases["case10"] = "insufficient_producer_degradation_cancel_no_stop"
+    test_cases["case11"] = "insufficient_producer_degradation_cancel_and_stop"
+    test_cases["case12"] = "inactive_crc_test"
+    test_cases["case13"] = "cross_normal_test"
 
     for i in range(1, 14):
         Logger.info("Begin testing case{}".format(i))
-        os.system("python3 case{}.py > ./datas/server_test_result/case{}.log".format(i, i))
+        case = test_cases["case{}".format(i)]
+        os.system("python3 {}.py > ./datas/server_test_result/{}.log".format(case, case))
         Logger.info("Finish testing case{}".format(i))
 
 
