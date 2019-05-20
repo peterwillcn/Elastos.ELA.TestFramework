f77be12b2c19717211519f47a91461e21684b28c
liteng <liteng@elastos.org>
2019-05-20 11:12:01 +0800

    add income distribution checking in dpos normal test

--- dpos_normal_test.py
+++ dpos_normal_test.py
@@ -12,9 +12,9 @@ config = {
     "ela": {
         "enable": True,
         "password": "123",
-        "number": 32,
-        "crc_number": 8,
-        "later_start_number": 8,
+        "number": 16,
+        "crc_number": 4,
+        "later_start_number": 4,
         "pre_connect_offset": 5,
         "crc_dpos_height": 300,
         "public_dpos_height": 308
@@ -30,12 +30,7 @@ def test_content():
     h1 = controller.params.ela_params.crc_dpos_height
     h2 = controller.params.ela_params.public_dpos_height
     pre_offset = config["ela"]["pre_connect_offset"]
-    number = controller.params.ela_params.number
     crc_number = controller.params.ela_params.crc_number
-    later_start_number = controller.params.ela_params.later_start_number
-
-    # init later start nodes include both registered and normal
-    later_start_nodes = controller.node_manager.ela_nodes[number - later_start_number + 1: number + 1]
 
     current_height = controller.get_current_height()
     if current_height < h1 - pre_offset - 1:
@@ -46,6 +41,12 @@ def test_content():
 
     global result
     global start_height
+    global after_h2_transactions
+    global last_income_height
+    global dpos_votes
+    dpos_votes = dict()
+    last_income_height = h2
+    after_h2_transactions = list()
     start_height = 0
 
     while True:
@@ -67,13 +68,24 @@ def test_content():
         if current_height == h2 + 2:
             Logger.info("H2 PASS!")
             Logger.info("H2 PASS!")
+            dpos_votes = controller.get_dpos_votes()
+
+        if current_height > h2 and controller.has_dpos_reward(current_height):
+            tx_fee = controller.get_total_tx_fee(after_h2_transactions)
+            real_income = controller.get_dpos_real_income(current_height)
+            theory_income = controller.get_dpos_theory_income(current_height - last_income_height, tx_fee, dpos_votes)
+            result = controller.check_dpos_income(real_income, theory_income)
+            controller.check_result("check dpos income", result)
+            after_h2_transactions.clear()
+            last_income_height = current_height
+            dpos_votes = controller.get_dpos_votes()
 
         # current is equal 380, start the later nodes include two candidates and two normal nodes
-        if later_start_number != 0 and start_height == 0 and current_height > h2 + crc_number * 3 * 6:
+        if start_height == 0 and current_height > h2 + crc_number * 3 * 6:
             controller.start_later_nodes()
             start_height = current_height
 
-        if start_height != 0 and current_height > start_height + 500000:
+        if start_height != 0 and current_height > start_height + 500:
             result = controller.check_nodes_height()
             controller.check_result("check all the nodes height", result)
             break
