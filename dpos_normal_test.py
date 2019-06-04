91c77b6b702760e021b681d8ab7add77409264b5
liteng <liteng@elastos.org>
2019-06-04 10:23:12 +0800

    change to stable keystores

--- dpos_normal_test.py
+++ dpos_normal_test.py
@@ -86,7 +86,7 @@ def test_content():
             controller.start_later_nodes()
             start_height = current_height
 
-        if start_height != 0 and current_height > start_height + 24:
+        if start_height != 0 and current_height > start_height + 100:
             result = controller.check_nodes_height()
             controller.check_result("check all the nodes height", result)
             break
